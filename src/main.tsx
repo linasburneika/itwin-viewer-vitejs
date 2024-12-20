d587604e0c0dac02fd146a62169a03b06aacce06
Linas B <linas.burneika@bentley.com>
2024-12-20 14:50:05 +0200

    working vitejs project

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { Auth } from './Auth';
+
+Auth.initialize({
+  scope: import.meta.env.VITE_IMJS_AUTH_CLIENT_SCOPES,
+  clientId: import.meta.env.VITE_IMJS_AUTH_CLIENT_CLIENT_ID,
+  redirectUri: import.meta.env.VITE_IMJS_AUTH_CLIENT_REDIRECT_URI,
+  postSignoutRedirectUri: import.meta.env.VITE_IMJS_AUTH_CLIENT_LOGOUT_URI,
+  responseType: 'code',
+  authority: import.meta.env.VITE_IMJS_AUTH_AUTHORITY,
+});
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+const redirectUrl = new URL(import.meta.env.VITE_IMJS_AUTH_CLIENT_REDIRECT_URI);
+if (redirectUrl.pathname === window.location.pathname) {
+  Auth.handleSigninCallback().catch(console.error);
+} else {
+  console.log('rendering');
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
---
+++ vite.config.ts
@@ -0,0 +1,67 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr'
+import { NodePackageImporter } from 'sass';
+import fs from 'fs';
+import { parse, resolve} from 'path'
+import { replaceCodePlugin } from "vite-plugin-replace";
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
+  plugins: [
+    react(),
+    svgr({
+      include: "**/*.svg",
+    }),
+    replaceCodePlugin({
+      replacements: [
+        {
+          from: "__dirname",
+          to: "/",
+        }
+      ],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern',
+        importers: [new NodePackageImporter()],
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        // this is required for the SCSS 
+        // a very rough implementation of the SCSS tilde import
+        find: /^~(.*)$/,
+        replacement: '$1',
+        customResolver(source, importer) {
+          if (fs.existsSync(source)) {
+            return source;
+          }
+
+          if (importer?.endsWith('.scss') || importer?.endsWith('.sass')) {
+            const dir = parse(source).dir
+            const fileName = parse(source).name;
+            const ext = parse(source).ext || '.scss';
+
+            const partial = resolve('node_modules', dir, `_${fileName}${ext}`);
+            const scss = resolve('node_modules', dir, `${fileName}${ext}`);
+
+            if (fs.existsSync(partial)) {
+              return partial;
+            }
+            if (fs.existsSync(scss)) {
+              return scss;
+            }
+          }
+        },
+      },
+    ],
+  },
+});
